2e25575067fbc56abaf5843e51afde8fe0bf4848
Rachel Porter <rachelandrewsporter@gmail.com>
2016-01-19 15:34:41 -0500

    adds scrollspy and tabs and parallax

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+    $('.parallax').parallax();
+    $('ul.tabs').tabs();
+
+    var options = [
+      {selector: '#projects', offset: 300, callback: 'showNavbar()'}
+    ];
+
+    Materialize.scrollFire(options);
+  });
+
+  function showNavbar(){
+    $('.navbar-fixed').show();
+  }
